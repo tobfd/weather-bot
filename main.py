3d566cfc3ec44ed636abe7ec8e6e473d3df8e0d4
Tobias Schmitt <tobias.schmitt2009@gmail.com>
2024-07-14 22:10:57 +0200

    refactor: improvements

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from dotenv import load_dotenv
+import os
+
 import discord
 import ezcord
-
-import os
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 bot = ezcord.Bot(intents=discord.Intents.all())
 bot.add_help_command()
-bot.add_status_changer([discord.CustomActivity(name="💻 Kevins Hackathon"), discord.CustomActivity(name="🌤️ Hier siehst du das Wetter")], interval=15)
+bot.add_status_changer(
+    [
+        discord.CustomActivity(name="💻 Kevins Hackathon"),
+        discord.CustomActivity(name="🌤️ Hier siehst du das Wetter"),
+    ],
+    interval=15,
+)
 
 
 bot.load_cogs()
 bot.run()
-
