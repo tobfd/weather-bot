bde045c5b0c47e0c15f1f1d1adea331f655a5b63
Tobias Schmitt <tobias.schmitt2009@gmail.com>
2024-07-14 22:09:39 +0200

    refactor: add codeblocks

--- cogs/weather.py
+++ cogs/weather.py
@@ -1,19 +1,27 @@
+import os
+
 import discord
+import ezcord
 import pyowm.commons.exceptions
 from discord import Interaction
+from discord.commands import option, slash_command
+from dotenv import load_dotenv
+from ezcord import codeblock
 from pyowm import OWM
 from pyowm.utils import timestamps
 from pyowm.weatherapi25 import Weather as WeatherClass
-import ezcord
-from ezcord import codeblock
 from translate import Translator
-from discord.commands import option, slash_command
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 API_KEY = os.getenv("KEY")
-COLORS_BY_TEMP = {35: discord.Color.dark_red(), 30: discord.Color.red(), 25: discord.Color.dark_orange(), 20: discord.Color.orange(), 10: discord.Color.blue(), 0: discord.Color.dark_blue()}
+COLORS_BY_TEMP = {
+    35: discord.Color.dark_red(),
+    30: discord.Color.red(),
+    25: discord.Color.dark_orange(),
+    20: discord.Color.orange(),
+    10: discord.Color.blue(),
+    0: discord.Color.dark_blue(),
+}
 
 owm = OWM(API_KEY)
 mgr = owm.weather_manager()
@@ -22,7 +30,13 @@ CRAD = "°C"
 translator = Translator(to_lang="de")
 
 
-async def get_view(city: str, sunrise: str, sunset: str, forecast: bool = False, original_weather: WeatherClass | None = None) -> discord.ui.View:
+async def get_view(
+    city: str,
+    sunrise: str,
+    sunset: str,
+    forecast: bool = False,
+    original_weather: WeatherClass | None = None,
+) -> discord.ui.View:
     if forecast:
         label = "Jetzt"
         style = discord.ButtonStyle.secondary
@@ -30,11 +44,15 @@ async def get_view(city: str, sunrise: str, sunset: str, forecast: bool = False,
         label = "Nächste 3 Stunden"
         style = discord.ButtonStyle.primary
     view = discord.ui.View(timeout=None, disable_on_timeout=True)
-    view.add_item(ForecastButton(city, label, sunrise, sunset, style=style, weather=original_weather))
+    view.add_item(
+        ForecastButton(city, label, sunrise, sunset, style=style, weather=original_weather)
+    )
     return view
 
 
-async def get_weather_embed(weather: WeatherClass, city: str, sunrise: str, sunset: str, three_hours: bool = False) -> discord.Embed:
+async def get_weather_embed(
+    weather: WeatherClass, city: str, sunrise: str, sunset: str, three_hours: bool = False
+) -> discord.Embed:
     temperaturen = weather.temperature("celsius")
     temp = round(int(temperaturen.get("temp")))
     color = discord.Color.orange()
@@ -42,24 +60,39 @@ async def get_weather_embed(weather: WeatherClass, city: str, sunrise: str, suns
         if temp >= temperature:
             color = COLORS_BY_TEMP[temperature]
             break
-    embed = discord.Embed(title=f"🌤️ Wetter in {city.title()}{" in 3 Stunden" if three_hours else ""}", color=color)
-    embed.add_field(name="🌡️ Temperatur", value=str(temp) + CRAD)
-    embed.add_field(name="🌡️ Fühlt sich an wie", value=str(round(int(temperaturen.get("feels_like")))) + CRAD)
+    embed = discord.Embed(
+        title=f"🌤️ Wetter in {city.title()}{" in 3 Stunden" if three_hours else ""}", color=color
+    )
+    embed.add_field(name="🌡️ Temperatur", value=codeblock(str(temp) + CRAD))
+    embed.add_field(
+        name="🌡️ Fühlt sich an wie",
+        value=codeblock(str(round(int(temperaturen.get("feels_like")))) + CRAD),
+    )
     embed.add_field(name="", value="")  # Kategorisierung
     embed.add_field(name="", value="", inline=False)  # Abstand von Zeile 1 bis Zeile 2
-    embed.add_field(name="💧 Luftfeuchtigkeit", value=str(weather.humidity) + "%")
-    embed.add_field(name="🍃 Wind Geschwindigkeit", value=str(round(int(weather.wind("km_hour").get("speed")))) + " Km/h")
+    embed.add_field(name="💧 Luftfeuchtigkeit", value=codeblock(str(weather.humidity) + "%"))
+    embed.add_field(
+        name="🍃 Wind Geschwindigkeit",
+        value=codeblock(str(round(int(weather.wind("km_hour").get("speed")))) + " Km/h"),
+    )
     embed.add_field(name="", value="")  # Kategorisierung
     embed.add_field(name="", value="", inline=False)  # Abstand von Zeile 1 bis Zeile 2
     embed.add_field(name="🌄 Sonnenaufgang", value=sunrise)
     embed.add_field(name="🌇 Sonnenuntergang", value=sunset)
     embed.add_field(name="", value="")  # Kategorisierung
-    embed.set_footer(text=translator.translate(weather.detailed_status), icon_url=weather.weather_icon_url())
+    embed.set_footer(
+        text=translator.translate(weather.detailed_status), icon_url=weather.weather_icon_url()
+    )
     embed.set_thumbnail(url=weather.weather_icon_url())
     return embed
 
 
-class Weather(ezcord.Cog, group="Weather", emoji="🌤️", description="Mit diesen Commands wirst du immer übers Wetter informiert."):
+class Weather(
+    ezcord.Cog,
+    group="Weather",
+    emoji="🌤️",
+    description="Mit diesen Commands wirst du immer übers Wetter informiert.",
+):
     def __init__(self, bot: ezcord.Bot):
         self.bot = bot
 
@@ -84,7 +117,15 @@ def setup(bot):
 
 
 class ForecastButton(discord.ui.Button):
-    def __init__(self, city: str, label: str, sunrise: str, sunset: str, weather: WeatherClass, style: discord.ButtonStyle | None = discord.ButtonStyle.primary):
+    def __init__(
+        self,
+        city: str,
+        label: str,
+        sunrise: str,
+        sunset: str,
+        weather: WeatherClass,
+        style: discord.ButtonStyle | None = discord.ButtonStyle.primary,
+    ):
         super().__init__(label=label, style=style, custom_id="forecast_button")
         self.city = city
         self.sunrise = sunrise
@@ -101,6 +142,14 @@ class ForecastButton(discord.ui.Button):
             response: WeatherClass = prognose.get_weather_at(timestamps.next_three_hours())
             weather = response
             three_hours = True
-        embed = await get_weather_embed(weather, self.city, sunrise=self.sunrise, sunset=self.sunset, three_hours=three_hours)
-        view = await get_view(self.city, self.sunrise, self.sunset, forecast=three_hours, original_weather=self.weather)
+        embed = await get_weather_embed(
+            weather, self.city, sunrise=self.sunrise, sunset=self.sunset, three_hours=three_hours
+        )
+        view = await get_view(
+            self.city,
+            self.sunrise,
+            self.sunset,
+            forecast=three_hours,
+            original_weather=self.weather,
+        )
         await interaction.edit(embed=embed, view=view)
